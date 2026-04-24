e02519fc9f109201a556bdb1e1c7aa6b2fd4ddbc
Shreyash <shreyash1833@gmail.com>
2026-04-24 15:12:33 +0530

    new changes added

--- jmasale/jmasale/settings.py
+++ jmasale/jmasale/settings.py
@@ -80,7 +80,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'jmasale.middleware.APITimingMiddleware',
     'django.middleware.common.CommonMiddleware',
